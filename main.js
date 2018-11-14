9763b04e1a00b34167a0e89c5412bcc7674d4612
Laurent Bassin <laurent@bassin.info>
2018-11-14 20:29:22 +0100

    Add prop_access

--- main.js
+++ main.js
@@ -6,3 +6,26 @@ let helloComponent = new HelloComponentClass();
 let returned = helloComponent.display({
     'toto': 'hello world'
 });
+
+Object.prototype.prop_access = function (path) {
+    let obj = this;
+    if (!path) {
+        return this;
+    }
+
+    let notFound = false;
+    path.split('.').map(key => {
+        if (!obj.hasOwnProperty(key)) {
+            notFound = true;
+            return;
+        }
+
+        obj = obj[key];
+    });
+
+    if (notFound) {
+        throw new Error(`Property ${path} does not exists`)
+    }
+
+    return obj;
+};
