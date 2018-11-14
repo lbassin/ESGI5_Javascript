864da23173eb8498affd61de5d6538e89d6a58dd
François MATHIEU <francois.mathieu@livexp.fr>
2018-11-14 18:17:34 +0100

    Add base component model

---
+++ src/Component.js
@@ -0,0 +1,19 @@
+function Component() {
+    this.props = {};
+    this.maraviable = null;
+
+
+    function display(newProps) {
+        shouldUpdate(newProps)
+    }
+
+    function shouldUpdate(newProps) {
+        //TODO compare newProps with this.props
+        //TODO if true, call this.render()
+    }
+
+
+    function render() {
+        //TODO call sub-components display method
+    }
+}
