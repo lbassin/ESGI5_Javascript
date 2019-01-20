c395236f8722fdfb25c3f6a020a18402fb3e3057
Laurent Bassin <laurent@bassin.info>
2019-01-20 18:40:40 +0100

    Exec javascript script after last render

--- main.js
+++ main.js
@@ -4,4 +4,20 @@ import RootComponent from "./src/RootComponent.js";
 const rootComponent = new RootComponent();
 rootComponent.display().then((content) => {
     document.getElementById('root').innerHTML = content;
+
+    execScripts(content);
 });
+
+function execScripts(content) {
+    const regex = new RegExp('<script(.+?)<\\/script>', 'gis');
+    const data = content.match(regex);
+
+    for (let i = 0; i < data.length; i++) {
+        const startOfScript = data[i].indexOf('>') + 1;
+        const scriptLength = data[i].lastIndexOf('<') - startOfScript;
+
+        const script = data[i].substr(startOfScript, scriptLength);
+
+        eval(script);
+    }
+}
